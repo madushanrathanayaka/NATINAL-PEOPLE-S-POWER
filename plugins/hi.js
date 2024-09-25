cb545d75574e14394b3d25e81f5fdcd702409441
madushanrathnayakasl <jaliyamadushan21@gmail.com>
2024-09-25 22:11:16 +0530

    Update hi.js

--- plugins/hi.js
+++ plugins/hi.js
@@ -6,7 +6,7 @@ cmd({
     alias: ["panel"],
     desc: "Get bot\'s command list.",
     category: "main",
-    use: 'hi',
+    use: 'Hi',
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
